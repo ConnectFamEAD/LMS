7766872af21c553e8c4305aa05f96309ec3de523
matheusmiguel-santos <149403481+conectfam@users.noreply.github.com>
2023-11-06 12:40:10 -0300

    s

---
+++ config/dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MONGO_URI: "mongodb+srv://conectfam_LMS:%40desenho1977@cluster0.fsgtwrj.mongodb.net/?retryWrites=true&w=majority",
+    SECRET_KEY: "@secret1997" // Substitua pelo valor que desejar
+  };
+  
