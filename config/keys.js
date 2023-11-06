12a37d18b34afc075b9948a23ea5abeed394f7f8
matheusmiguel-santos <149403481+conectfam@users.noreply.github.com>
2023-11-06 12:49:56 -0300

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,9 @@
-if(process.env.NODE_ENV==='production'){
-    module.exports=require('./prod')
-}
-else{
-    module.exports=require('./dev')
-}
+if (process.env.NODE_ENV === 'production') {
+    module.exports = {
+      MONGO_URI: process.env.MONGO_URI,
+      SECRET_KEY: process.env.SECRET_KEY
+    };
+  } else {
+    module.exports = require('./dev'); // Apenas em desenvolvimento
+  }
+  
