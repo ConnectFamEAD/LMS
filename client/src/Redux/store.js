790108caecaf325505cadefd397fd4d7e389e388
matheusmiguel-santos <149403481+conectfam@users.noreply.github.com>
2023-11-05 21:05:30 -0300

    front end removido

--- client/src/Redux/store.js
+++
@@ -1,8 +0,0 @@
-import { applyMiddleware, createStore } from 'redux'
-import rootReducer from './rootReduer'
-import Thunk from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension';
-
-const store=createStore(rootReducer,composeWithDevTools(applyMiddleware(Thunk)))
-
-export default store
